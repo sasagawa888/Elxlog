8043bb196a55d7c67e8de378981025ca5d7d744e
sasagawa888 <ken-s@tkcnf.or.jp>
2019-12-01 16:56:09 +0900

    Modify mix.exs elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elxlog.MixProject do
     [
       app: :elxlog,
       version: "0.1.4",
-      elixir: "~> 1.7",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
